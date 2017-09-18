50d47f227f095fff2babf588d9ed855dfd3432a2
java <java@bigdata>
2017-09-18 10:09:55 +0900

    first commit

---
+++ src/main/java/com/example/Chap04Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap04Application.class, args);
+	}
+}
